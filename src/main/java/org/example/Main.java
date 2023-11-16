09e2ef457245e7c89d1120c31e527d50be996d53
dermo <dermot.logue@dkit.ie>
2023-11-16 11:10:53 +0000

    added default method (commented out)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,18 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("IMeasurableContainer Interface");
+
+        ContainerManager manager = new ContainerManager();
+        manager.add( new Box(.4,0.2,0.3,2.55));
+        manager.add( new Cylinder(.5,.8,1.50));
+
+        double totalWeight = manager.totalWeight();
+        System.out.println("Total weight = " + totalWeight);
+
+        double totalRectangularVolume = manager.totalRectangularVolume();
+        System.out.println("Total volume = " + totalRectangularVolume);
+
     }
 }
